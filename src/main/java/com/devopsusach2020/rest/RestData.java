a16c9c4d9d130ad30fb321cd862b06dddfed2a9e
LAPTOP-776AMTE0\carlo.figueroa <carlo.figueroa@sistemassa.cl>
2020-11-30 23:13:38 -0300

    Correccion Quality Gate 1

--- src/main/java/com/devopsusach2020/rest/RestData.java
+++ src/main/java/com/devopsusach2020/rest/RestData.java
@@ -46,15 +46,22 @@ public class RestData {
 		int death = 0;
 		int recovered = 0;
 		Gson gson = new Gson();
-        Pais[] estados = gson.fromJson(call.getBody().toLowerCase(), Pais[].class);
+		
 
-        for(Pais estado : estados) {
-        	response.setDate(estado.getDate());
-        	response.setActive(estado.getActive());
-        	confirmed += estado.getConfirmed();
-        	death += estado.getDeaths();
-        	recovered += estado.getRecovered();
-        }
+		try {
+            Pais[] estados = gson.fromJson(call.getBody().toLowerCase(), Pais[].class);
+
+            for (Pais estado : estados) {
+                response.setDate(estado.getDate());
+                response.setActive(estado.getActive());
+                confirmed += estado.getConfirmed();
+                death += estado.getDeaths();
+                recovered += estado.getRecovered();
+            }
+        } catch(NullPointerException ex){
+			LOGGER.log(Level.SEVERE, "Consulta por pais nula");
+		}
+
         
     	response.setConfirmed(confirmed);
     	response.setDeaths(death);
